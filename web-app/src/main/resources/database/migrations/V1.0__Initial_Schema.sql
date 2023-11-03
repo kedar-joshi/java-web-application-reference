4cda0631ed5a95c45cf88ccb77072e0be45b2492
Kedar Joshi <kedar-joshi@users.noreply.github.com>
2023-11-03 23:20:11 +0530

    Configured schema versioning with Flyway

---
+++ web-app/src/main/resources/database/migrations/V1.0__Initial_Schema.sql
@@ -0,0 +1,12 @@
+-- Java Web Application v1.0.0
+
+-- Initialization
+
+-- DROP DATABASE IF EXISTS "java-web-application";
+-- CREATE DATABASE "java-web-application" WITH ENCODING 'UTF-8';
+
+-- Extensions
+
+-- UUID
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA "public";
+
